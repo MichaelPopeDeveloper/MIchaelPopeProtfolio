d10d64e3cdf651244e4ef02a633ebb03c905e3cd
Michael <michaelpopedeveloper@gmail.com>
2018-11-02 23:06:06 -0700

    node

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ const path = require('path');
 const app = express();   
 
 // Serve only the static files form the dist directory    
-app.use(express.static(__dirname + '/dist'));
+app.use(express.static(__dirname + '/dist/portfolio2'));
 
 app.get('/*', function(req,res) {  
     res.sendFile(path.join(__dirname+'/dist/portfolio2/index.html'));   

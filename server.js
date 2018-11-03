31401af8f34592c4f333be43bee29b473fb0be1a
Michael <michaelpopedeveloper@gmail.com>
2018-11-02 23:05:38 -0700

    node

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ const app = express();
 app.use(express.static(__dirname + '/dist'));
 
 app.get('/*', function(req,res) {  
-    res.sendFile(path.join(__dirname+'/dist/index.html'));   
+    res.sendFile(path.join(__dirname+'/dist/portfolio2/index.html'));   
 });  
 
 // Start the app by listening on the default Heroku port    
